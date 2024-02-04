d72a159554a8e4aa1eb6271d22c3c4203700fe07
GoTierGod <ivan.gunlove_10_@hotmail.com>
2024-02-04 05:56:21 -0300

    Middleware

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 import express from 'express'
+import morgan from 'morgan'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
+// Config
 app.set('x-powered-by', false)
 app.set('json scape', true)
 app.set('json spaces', 4)
 app.set('strict routing', false)
 
+// Middleware
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Get method
 app.get('/', (req, res) => {
-    res.send(app.locals)
+    res.json({
+        message: 'hello world',
+    })
 })
 
-app.listen(port)
+app.use((req, res) => {
+    res.status(404).json({ message: 'not found' })
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
